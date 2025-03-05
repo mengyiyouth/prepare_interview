d5e697aac412b11bcd946b3242249d54efaa735f
Mengyi <mengyiyouth@gmail.com>
2025-03-05 11:34:01 +0800

    The singleton implementation(Enum, double-check, lazy, eager, InnerHolder)

---
+++ src/main/java/com/mengyi/designpatterns/singleton/EagerSingleton.java
@@ -0,0 +1,17 @@
+package com.mengyi.designpatterns.singleton;
+
+public class EagerSingleton {
+    private static final EagerSingleton INSTANCE = new EagerSingleton();
+
+    private EagerSingleton(){}
+
+    public static EagerSingleton getInstance(){
+        return INSTANCE;
+    }
+
+    public static void main(String[] args) {
+        EagerSingleton instance1 = EagerSingleton.getInstance();
+        EagerSingleton instance2 = EagerSingleton.getInstance();
+        System.out.println(instance1 == instance2);
+    }
+}
